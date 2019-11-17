27ec0cb6246aff8c9074956439ac41d11b1dc47b
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-11-17 23:00:11 +0300

    Init

---
+++ Utils/IToken.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomFramework.Authorization.Utils
+{
+    public interface IToken
+    {
+        string Issuer { get; set; }
+        string Audience { get; set; }
+        string Key { get; set; }
+        int ExpireInMinutes { get; set; }
+    }
+}
